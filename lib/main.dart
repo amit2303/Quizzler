6d42ad65c486aada9320afc006551f6132f44f17
Amit Mishra <amitmishra0323@gmail.com>
2023-10-08 19:04:05 +0530

    quiz_brain.dart  abstraction

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'questions.dart';
-// import 'quiz_brain.dart';
+// import 'questions.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -43,14 +43,17 @@ class _QuizPageState extends State<QuizPage> {
   //   true,
   //   false];
 
-  //Step -2
-List<Question> questionBank = [
-Question('1+1 =4?', false),
-Question('4*3 =12?', true),
-Question('India\'s capital is Mumbai.', false),
-];
+//   //Step -2
+// List<Question> questionBank = [
+// Question('1+1 =4?', false),
+// Question('4*3 =12?', true),
+// Question('India\'s capital is Mumbai.', false),
+// ];
 
+//step -3  Abstraction
 
+//concept of abstraction
+ QuizBrain quizBrain = QuizBrain();
 
 
   List<Icon> scoreKeeper = [];
@@ -70,7 +73,8 @@ Question('India\'s capital is Mumbai.', false),
             child: Center(
               child: Text(
                 // questions[que_no],   //step -1
-                questionBank[que_no].queText,
+                // questionBank[que_no].queText, //step-2
+                quizBrain.questionBank[que_no].queText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -97,7 +101,8 @@ Question('India\'s capital is Mumbai.', false),
               onPressed: () {
                 setState(() {
                   // bool correctans = answer[que_no];   //Step-1
-                  bool correctans = questionBank[que_no].correctAns;
+                  // bool correctans = questionBank[que_no].correctAns; //step-2
+                  bool correctans = quizBrain.questionBank[que_no].correctAns;
                   if (correctans == true) {
                     print("correct");
                   } else {
@@ -107,7 +112,7 @@ Question('India\'s capital is Mumbai.', false),
                     Icons.check,
                     color: Colors.lightGreenAccent,
                   ));
-                  if (que_no < 2) {
+                  if (que_no < 12) {
                     que_no = que_no + 1;
                   } else {
                     print("Test Finished");
@@ -132,7 +137,8 @@ Question('India\'s capital is Mumbai.', false),
               onPressed: () {
                 setState(() {
                   // bool correctans = answer[que_no]; //step-1
-                  bool correctans = questionBank[que_no].correctAns;
+                  // bool correctans = questionBank[que_no].correctAns;  //step -2
+                  bool correctans = quizBrain.questionBank[que_no].correctAns;
                   if (correctans == false) {
                     print("correct");
                   } else {
@@ -142,7 +148,7 @@ Question('India\'s capital is Mumbai.', false),
                     Icons.close,
                     color: Colors.red,
                   ));
-                  if (que_no < 2) {
+                  if (que_no < 12) {
                     que_no = que_no + 1;
                   } else {
                     print("Test Finished");
